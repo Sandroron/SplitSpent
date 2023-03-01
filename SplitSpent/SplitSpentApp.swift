07ab630d6c50a4477fab323248d0134051f47b4f
Oleksandr Razumnyi <u1133489@internal.imsglobal.com>
2023-03-01 20:56:07 +0200

    Initial Commit

---
+++ SplitSpent/SplitSpentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitSpentApp.swift
+//  SplitSpent
+//
+//  Created by CT Razumnyi on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SplitSpentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
